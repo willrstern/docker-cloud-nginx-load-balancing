69feb61347986548f65240bb9cf86aba14eff5d1
Will Stern <willrstern@gmail.com>
2015-12-12 05:32:54 -0600

    added babel transpilation

--- tutum-watch.js
+++ tutum-watch.js
@@ -1,15 +1,2 @@
-const reloadNginx = require('./lib/reload-nginx');
-const tutum = require('./lib/tutum');
-
-//Build Initial Config
-reloadNginx();
-
-tutum.stream.on('open', () => console.log('Listening for Tutum Change Events'));
-tutum.stream.on('error', (message) => console.log('Tutum Stream Error: %s', message));
-tutum.stream.on('close', () => console.log('Tutum Socket closed', arguments));
-tutum.stream.on('message', (message) => {
-  message = JSON.parse(message);
-  if (message.state === "Running" && ~["container", "service"].indexOf(message.type)) {
-    reloadNginx();
-  }
-});
+require('babel-register');
+require('./lib/tutum-watch');
