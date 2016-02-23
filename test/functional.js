8de4eaec36aa1bb39546569eb5b1fc9fa3bf3576
Will Stern <willrstern@gmail.com>
2016-02-23 16:02:48 -0600

    added functional tests

---
+++ test/functional.js
@@ -0,0 +1,117 @@
+import assert from "assert"
+import { execSync } from "child_process"
+import { readFileSync } from "fs"
+import { find } from "lodash"
+import nginxConfigParser from "nginx-config-parser"
+
+import { getContainersToBalance, parseServices, generateNewConfig } from "../lib/reload-nginx"
+
+const NGINX_CONFIG_FILE = "/etc/nginx/conf.d/default.conf";
+
+describe("nginx", () => {
+  beforeEach(function(done) {
+    const containers = getContainersToBalance(require("./mocks/allcontainers.json"))
+    const services = parseServices(containers)
+    const newConfig = generateNewConfig(services)
+
+    //a little time for checksum
+    setTimeout(() => {
+      done()
+    }, 100)
+  })
+
+  describe("config", () => {
+    it("was created", () => {
+      readFileSync(NGINX_CONFIG_FILE);
+    })
+
+    it("is accepted by nginx", () => {
+      execSync("nginx -t")
+    })
+
+    describe("upstreams", () => {
+      it("contains running test.com upstream nodes", () => {
+        const config = nginxConfigParser.parseFromString( readFileSync(NGINX_CONFIG_FILE, 'utf-8') )
+        const upstream = config["upstream test.com"][0].server;
+
+        assert(hasUpstream(upstream, "10.7.0.1:3000"))
+        assert(hasUpstream(upstream, "10.7.0.2:3001"))
+        assert(hasUpstream(upstream, "10.7.0.3:3002"))
+        assert(!hasUpstream(upstream, "10.7.0.4:3000"))
+      })
+
+      it("contains running test2.com upstream nodes", () => {
+        const config = nginxConfigParser.parseFromString( readFileSync(NGINX_CONFIG_FILE, 'utf-8') )
+        const upstream = config["upstream test2.com"][0].server;
+
+        assert(hasUpstream(upstream, "10.7.0.1:3000"))
+        assert(hasUpstream(upstream, "10.7.0.2:3001"))
+        assert(hasUpstream(upstream, "10.7.0.3:3002"))
+        assert(!hasUpstream(upstream, "10.7.0.4:3000"))
+      })
+
+      it("contains running test3.com upstream nodes", () => {
+        const config = nginxConfigParser.parseFromString( readFileSync(NGINX_CONFIG_FILE, 'utf-8') )
+        const upstream = config["upstream test3.com"][0].server;
+
+        assert(hasUpstream(upstream, "10.7.0.6:80"))
+      })
+
+      it("contains running test4.com upstream nodes", () => {
+        const config = nginxConfigParser.parseFromString( readFileSync(NGINX_CONFIG_FILE, 'utf-8') )
+        const upstream = config["upstream test4.com"][0].server;
+
+        assert(hasUpstream(upstream, "10.7.0.7:80"))
+      })
+
+      it("contains running test5.com upstream nodes", () => {
+        const config = nginxConfigParser.parseFromString( readFileSync(NGINX_CONFIG_FILE, 'utf-8') )
+        const upstream = config["upstream test5.com"][0].server;
+
+        assert(hasUpstream(upstream, "10.7.0.8:80"))
+      })
+
+      it("contains running test6.com upstream nodes", () => {
+        const config = nginxConfigParser.parseFromString( readFileSync(NGINX_CONFIG_FILE, 'utf-8') )
+        const upstream = config["upstream test6.com"][0].server;
+
+        assert(hasUpstream(upstream, "10.7.0.8:80"))
+      })
+    })
+
+    describe("servers", () => {
+      it("proxy_pass points to correct upstream for virtual hosts", () => {
+        const config = nginxConfigParser.parseFromString( readFileSync(NGINX_CONFIG_FILE, 'utf-8') )
+        const hosts = ["test.com", "test2.com", "test3.com", "test4.com", "test5.com", "test6.com"];
+
+        hosts.forEach((host) => {
+          assert.equal(getProxyPass(config.server, host), `http://${host}`);
+        })
+      })
+    })
+  })
+
+})
+
+function hasUpstream(upstream, url) {
+  let exists = false;
+  upstream.forEach((val) => {
+    if (Array.isArray(val) && val[0] === url) {
+      exists = true;
+    }
+  })
+  return exists;
+}
+
+function getProxyPass(servers, serverName) {
+  const server = getServer(servers, serverName);
+  return server["location /"][0].proxy_pass[0][0];
+}
+
+function getServer(servers, serverName) {
+  for (let i=0; i < servers.length; i++) {
+    if (servers[i].server_name[0][0] === serverName) {
+      return servers[i]
+    }
+  }
+}
--- test/reload-nginx.test.js
+++ test/reload-nginx.test.js
@@ -3,6 +3,7 @@ import { readFileSync } from "fs"
 import { find } from "lodash"
 
 import { getContainersToBalance, parseServices, generateNewConfig } from "../lib/reload-nginx"
+const certString = "-----BEGIN RSA PRIVATE KEY-----\\nMIICXQIBAAKBgQC/G6eud0cQqqI729F+uxT1Zv1HLpqz/qOc/3GjtZohNUyOCJK0\\ngoiiNwz9nNy62Q3iAknW/EP6TKNfiN1AFNsqECsxOku0mUAyvERLeBfWpXVXTi27\\n7ml7KXoVhhDxPj3/PDsgyvrEsQ8e9/jDvkzovSKyfMJa0shbi6gwzJr+awIDAQAB\\nAoGBAK0yoBZzDVnieyOqxcOIQ6dgjlzrtNM6DQglTdVjqWs9RcNXq7Wis7foEoLq\nnfVM79ML5eXMPMNkn4/elz4TaMe1tQKzeevy7waLEjLDlrtqQs4duX4ulUhQvDr2\\nZnWLiaoGIN/K+QnHzR1k7Kj07sT8PL3gIwqtqRdDBSO4ljIRAkEA6wMZ/OxKJCH6\nlKQ3C+7wlXttmBhkGQpzjvC0E8yJFR2ui/SCnvL75OD5+2pDUGpoXaIeCXDAisWN\\nAi/KZ9bF2QJBANAszwEUeU1cjf6vPTxrLOzcVLZSdgr3NBe8IAUtE46jL3rdfWlQ\nAYPmXfmRynLk8hZM3LgJFM1gO8JrFTJBp+MCQE3UUR76AfPFbP8dAz3oe7SFk93y\\n9fN1CqAkBv8nlZ5wngWrjDansdQyzZb9sh1HoBiiP+BQfvN2SSSYPyf0cMECQQC2\nkQV92fnDyc7Rs9eNXCTLGTPFra3OUhvCUP736x9CsYRbSVHKARtDFM4HqD8W4ggZ\\nXJEZaQVwU9w01fqB16inAkBW5FWZixHCqHrDHMKevB+VRGtr94s0yOAxUZPPEAT/\nwIBs4GGlzYk6sPgp8vMOHCtoox2JjzzgiZCsU2HObLS/\\n-----END RSA PRIVATE KEY-----\n-----BEGIN CERTIFICATE-----\\nMIIB8TCCAVoCCQCrK/E7Wz0CxTANBgkqhkiG9w0BAQUFADA9MQswCQYDVQQGEwJV\nUzELMAkGA1UECBMCVFgxITAfBgNVBAoTGEludGVybmV0IFdpZGdpdHMgUHR5IEx0\\nZDAeFw0xNjAyMjMxNzUxMzBaFw0xNzAyMjIxNzUxMzBaMD0xCzAJBgNVBAYTAlVT\\nMQswCQYDVQQIEwJUWDEhMB8GA1UEChMYSW50ZXJuZXQgV2lkZ2l0cyBQdHkgTHRk\\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/G6eud0cQqqI729F+uxT1Zv1H\\nLpqz/qOc/3GjtZohNUyOCJK0goiiNwz9nNy62Q3iAknW/EP6TKNfiN1AFNsqECsx\\nOku0mUAyvERLeBfWpXVXTi277ml7KXoVhhDxPj3/PDsgyvrEsQ8e9/jDvkzovSKy\\nfMJa0shbi6gwzJr+awIDAQABMA0GCSqGSIb3DQEBBQUAA4GBAAQG+EhyuEQleHRg\\nuZZnGKIYbeODAWTY4UOVNjV2AItHWk/yPDbPoxhj9e1iC7JdKHgJTLaJw0JzLuXx\\nmPvyczvXfORsTv0Isc3JH71xhZ2GLX10rhQKBzIzud6CwopFmfdAAM0/z4gJ67JZ\\noAPJD828GizKayML5BIu3hQSufiy\\n-----END CERTIFICATE-----"
 
 describe("getContainersToBalance", () => {
   const containers = getContainersToBalance(require("./mocks/allcontainers.json"))
@@ -66,39 +67,44 @@ describe("parseServices", () => {
   })
 
   it("creates a cert file when NGINX_CERTS is set", () => {
-    const test3Cert = readFileSync(process.env.NGINX_CERTS + "/test3.com.crt", "utf-8")
-    assert(test3Cert === "test3.com");
+    const test3Cert = readFileSync("/certs/test3.com.crt", "utf-8")
+    const expectedCert = readFileSync(__dirname + "/mocks/test3.com.crt", "utf-8")
+
+    assert.equal(test3Cert, expectedCert);
   })
 
   it("doesn't create a cert file when no NGINX_CERTS is set", () => {
-    let fail = false;
+    let noTest4Cert = false;
     try {
-      readFileSync(process.env.NGINX_CERTS + "/test4.com.crt", "utf-8")
+      readFileSync("/certs/test4.com.crt", "utf-8")
     } catch(e) {
-      fail = true
+      noTest4Cert = true
     }
-    assert(fail);
+    assert(noTest4Cert);
   })
 
   it("creates multiple, comma-separated cert files when NGINX_CERTS is set", () => {
-    const testCert = readFileSync(process.env.NGINX_CERTS + "/test.com.crt", "utf-8")
-    const test2Cert = readFileSync(process.env.NGINX_CERTS + "/test2.com.crt", "utf-8")
-    assert(testCert === "test.com" && test2Cert === "test2.com");
+    const testCert = readFileSync("/certs/test.com.crt", "utf-8")
+    const test2Cert = readFileSync("/certs/test2.com.crt", "utf-8")
+    const expectedTestCert = readFileSync(__dirname + "/mocks/test.com.crt", "utf-8")
+    const expectedTest2Cert = readFileSync(__dirname + "/mocks/test2.com.crt", "utf-8")
+
+    assert.equal(testCert, expectedTestCert)
+    assert.equal(test2Cert, expectedTest2Cert)
   })
 
   it("allows for an empty cert with comma-separated cert files", () => {
     let noTest5Cert = false;
     try {
-      readFileSync(process.env.NGINX_CERTS + "/test5.com.crt", "utf-8")
+      readFileSync("/certs/test5.com.crt", "utf-8")
     } catch(e) {
       noTest5Cert = true
     }
-    const test6Cert = readFileSync(process.env.NGINX_CERTS + "/test6.com.crt", "utf-8")
-    assert(noTest5Cert && test6Cert === "test6.com");
+    const test6Cert = readFileSync("/certs/test6.com.crt", "utf-8")
+    const expectedCert = readFileSync(__dirname + "/mocks/test6.com.crt", "utf-8")
+
+    assert(noTest5Cert)
+    assert.equal(test6Cert, expectedCert);
   })
 
-})
-
-describe("TODO: generateNewConfig", () => {
-  it("is pending")
 })
